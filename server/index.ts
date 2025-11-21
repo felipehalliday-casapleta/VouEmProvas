d7a081b46f8c33eddaf66da907772d4a89ee51b4
felipehalliday-casapleta <felipehalliday@gmail.com>
2025-11-21 17:29:45 -0300

    Update index.ts Fix: add missing Google GSI domains to CSP (style-src + frame-src)

--- server/index.ts
+++ server/index.ts
@@ -17,12 +17,13 @@ app.use((req, res, next) => {
       "default-src 'self' https://vep.casapletafilmes.com.br",
       "connect-src 'self' https://vep.casapletafilmes.com.br https://accounts.google.com https://www.google.com",
       "script-src 'self' https://accounts.google.com 'unsafe-inline'",
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      "font-src 'self' https://fonts.gstatic.com",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com https://www.gstatic.com",
       "img-src 'self' data: https:",
-      "frame-ancestors 'none'",
-      "base-uri 'self'",
+      "frame-src https://accounts.google.com",
+      "font-src 'self' https://fonts.gstatic.com",
       "form-action 'self' https://accounts.google.com",
+      "base-uri 'self'",
+      "frame-ancestors 'none'",
       "object-src 'none'"
     ].join("; ")
   );
