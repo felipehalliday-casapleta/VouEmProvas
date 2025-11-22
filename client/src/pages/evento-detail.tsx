df3ef73dc889a86090b8f261c01d374d9f2b586c
felipehalliday-casapleta <felipehalliday@gmail.com>
2025-11-21 21:29:52 -0300

    Update evento-detail.tsx - inclusão de status

--- client/src/pages/evento-detail.tsx
+++ client/src/pages/evento-detail.tsx
@@ -199,7 +199,11 @@ export default function EventoDetailPage() {
                       </SelectTrigger>
                       <SelectContent>
                         <SelectItem value="Em Andamento">Em Andamento</SelectItem>
-                        <SelectItem value="Pendente">Pendente</SelectItem>
+                        <SelectItem value="Elaborando Proposta">Elaborando Proposta</SelectItem>
+                        <SelectItem value="Em Análise (Diretoria de Provas)">Em Análise (Diretoria de Provas)</SelectItem>
+                        <SelectItem value="Aceito (Diretoria de Provas)">Aceito (Diretoria de Provas)</SelectItem>
+                        <SelectItem value="De Acordo (Diretor Geral)">De Acordo (Diretor Geral)</SelectItem>
+                        <SelectItem value="Aguardando Feedback (Cliente)">Aguardando Feedback (Cliente)</SelectItem>
                         <SelectItem value="Aprovado">Aprovado</SelectItem>
                         <SelectItem value="Recusado">Recusado</SelectItem>
                       </SelectContent>
